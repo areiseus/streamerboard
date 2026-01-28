d23b870fe6036dd113443959aff228d2a2bb6777
areiseus <62370356+areiseus@users.noreply.github.com>
2026-01-28 23:51:19 +0900

    Update streamer_data_repeater.js

--- api/streamer_data_repeater.js
+++ api/streamer_data_repeater.js
@@ -30,8 +30,10 @@ export default async function handler(req, res) {
                     const isLive = liveDetail && liveDetail.RESULT ? true : false;
 
                     // broad_no가 존재하면 방송 중으로 판단
-                    const livetag = await client.CHANNEL(item.id);
-                    const Liveon = livetag.RESULT === 1;
+                    //const livetag = await client.CHANNEL(item.id);
+                    //const Liveon = livetag.RESULT === 1;
+
+                    const Liveon = 1;
                     
                     // 방송국 정보 (애청자, 구독자)
                     const stationInfo = await client.channel.station(item.id);
